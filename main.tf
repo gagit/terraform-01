14f6c72d95113fe3c5e00661c04c7d8359770725
“Ruben <ruben.nieva@gmail.com>
2020-08-09 21:24:11 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  version = "~> 2.7"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "web" {  
+  ami = "ami-0ac80df6eff0e70b5"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "devops-one"
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
+
+output "private_ip" {
+  value = "${aws_instance.web.private_ip}"
+}
+
