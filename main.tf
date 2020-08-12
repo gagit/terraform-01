8b61aeb59e9426952f8c83f07033f39a1ed567b6
Ruben Nieva <rnieva@simtlix.com>
2020-08-12 00:25:50 -0300

    comment out tags and output

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ resource "aws_instance" "web" {
   ami = "ami-0ac80df6eff0e70b5"
   instance_type = "t2.micro"
   
-  tags = {
-    Name = "devops-one"
-  }
+  // tags = {
+  //   Name = "devops-one"
+  // }
 }
 
 output "public_ip" {
   value = "${aws_instance.web.public_ip}"
 }
 
-output "private_ip" {
-  value = "${aws_instance.web.private_ip}"
-}
+// output "private_ip" {
+//   value = "${aws_instance.web.private_ip}"
+// }
 
