9f3719e57665381f98c843d810d8209b925e2ffa
Gabriel <gabrielandrestoledo@gmail.com>
2021-08-26 10:54:25 -0300

    corrigiendo conflictos

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 
 Hola!! estos son script de inicialción en el uso de terraform.
 Happy codding! :)
-=======
-Esta pequeña práctica tiene como fin la incialización en Terraform.
-Saludos!
->>>>>>> de5d70a7fe08212706979cdfa22948fb62826c68
--- main.tf
+++ main.tf
@@ -1,37 +1,25 @@
 terraform {
-<<<<<<< HEAD
-  required_providers{
-    /*
+  required_providers {
+      /*
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 2.13.0"
     }
+    
     */
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
-=======
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
       version = "~> 3.48.0"
->>>>>>> de5d70a7fe08212706979cdfa22948fb62826c68
     }
   }
 }
 
-<<<<<<< HEAD
-provider "aws" {
-  region  = "us-east-2"
-=======
 variable "aws_region" {
   default="us-east-2"
 }
 
-
 provider "aws" {
   region  = var.aws_region
->>>>>>> de5d70a7fe08212706979cdfa22948fb62826c68
 }
 
 resource "aws_instance" "web" {  
@@ -45,13 +33,11 @@ resource "aws_instance" "web" {
    }
 }
 
-sgr-0c0f23e390eb43319
-
-
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AJ+m3KgVA/YxHhOqHp9H6g/KgADlYXqSVQz7f0TWgbBNsrgv6pHXwaeiNuFuvO+BpWYDybe6f69zQ/nWlSnVU3TaqvMC228WtD4XzB6LzDu2+IJegqazJ9Dq0X3BLsGGCUWQh/IpURpSt8vS3ZgKFpH8xMdqCjUEAVCZkBsdM8zR96h2ABQpICLHmg3VHxgUHOPeToe0cmhfN2sMzavJZzs7A4kNwGX1P7aPWVVTDH3HppwVAhUoT3kpCue5pV3LXem0EdsHaz0o+QUYi+5DfVVrpcO6qPejKQNey9uLKcMCg6HTVg2P4zqM3s628imSh8UTqJ37T65q9nHoo1WjxUL6daN6qieYxvCk4ZytcxDZPeBK2K2zfs1qMbExf1VaxKn+jUewP4H1DKLMgsj5sVulb4QAhTJFkyOWDJCMgfYWYcQPcauAwTji4KwqGdQc30fTypevZRV6Np79Dvvd6cSI6sNJ0dHiqMDalucjo++O518hgYIiJ5Jk10MWXU9TqtigJqo6wGnDT4nJ0aNa2/D2lw7IKMdS0u4j2KZLddyYATx0b3bO8zIqbODt13Xj31gbzXbQ75qCeSLHig40gs0dX/gD9Eh525YwUMf3+ZMO7TKLxjA9Y6wt1C0R/X6Q8E4Yxw== gabrielandrestoledo@gmail.com"
+  public_key = "ssh-rsa AJ+m=="
 }
+
 output "public_ip" {
   value = "${aws_instance.web.public_ip}"
 }
